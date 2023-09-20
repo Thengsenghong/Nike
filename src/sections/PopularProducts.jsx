dd2afe3c7a7969820bf8a1c924e7412156bd95e8
senghong <senghong.theng@sovannaphumlife.com>
2023-09-20 17:35:37 +0700

    complete hero and Popular product

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,6 +1,24 @@
-
+import { products } from '../constants';
+import PopularProductCard from '../components/PopularProductCard';
 const PopularProducts = () => {
-    return <div>PopularProducts</div>;
+    return (
+        <section id="products" className="max-container mx-sm:mt-12">
+            <div className="flex flex-col justify-start gap-5">
+                <h2 className="text-4xl font-palanquin font-bold">
+                    Our <span className="text-coral-red">Popular</span> products
+                </h2>
+                <p className="lg:max-w-lg mt-2 font-montserrat">
+                    Experience top-notch quality and style with our sought-after
+                    selections. Discover a world fof comfort, design, and value
+                </p>
+            </div>
+            <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-3 grid-cols-1 sm:gap-6 gap-14'>
+                {products.map((product) =>(
+                    <PopularProductCard key = {product.name} {...product} />
+                ))}
+            </div>
+        </section>
+    );
 };
 
 export default PopularProducts;
